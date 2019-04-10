9f99d4c8b3c5eeb12cd3b03b4d5e28dd34da6fff
Anuarder <anuar.ibraev97@gmail.com>
2019-04-11 00:08:39 +0600

    First setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,33 @@
 <template>
-  <HelloWorld />
+    <v-content>
+        <div class="hero-header">
+            <div class="hero-text">
+                <img
+                    width="100" 
+                    src="/img/hospital.png">
+                <h1 class="display-4 font-weight-bold">WELCOME TO MODERN HOSPITAL</h1>
+                <v-btn 
+                    class="mt-5"
+                    color="light-blue" 
+                    large 
+                    round
+                    dark>Sign up online</v-btn>
+            </div>
+        </div>
+    </v-content>
 </template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>
+<style scoped>
+.hero-header{
+    height: 100vh;
+    background-image: url('/img/hero-header.jpg');
+    background-position: center;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+}
+.hero-text{
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,98 @@
+<template>
+    <v-content>
+        <v-toolbar 
+            flat 
+            fixed
+            color="transparent">
+            <v-toolbar-title class="white--text headline">
+                Hospital
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-side-icon 
+                color="white" 
+                flat
+                @click="sidebar = !sidebar">
+            </v-toolbar-side-icon>
+        </v-toolbar>
+        <v-navigation-drawer 
+            v-model="sidebar"
+            app
+            temporary
+            right>
+            <div class="sidebar">
+                <div class="sidebar-title">
+                   <v-list>
+                       <v-list-tile avatar>
+                           <v-list-tile-avatar>
+                                <v-icon color="red">
+                                    local_hospital
+                                </v-icon>
+                            </v-list-tile-avatar>
+                           <v-list-tile-content class="headline">
+                               Modern Hospital
+                           </v-list-tile-content>
+                       </v-list-tile>
+                   </v-list>
+                </div>
+                <div class="menu">
+                    <v-list>
+                        <v-list-tile 
+                            avatar 
+                            v-for="item in menu" 
+                            :key="item.path">
+                            <v-list-tile-avatar>
+                                <v-icon color="primary">
+                                    {{item.icon}}
+                                </v-icon>
+                            </v-list-tile-avatar>
+                            <v-list-tile-content>
+                                <v-list-tile-title class="title">
+                                    {{item.name}}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </v-list>
+                </div>
+            </div>
+        </v-navigation-drawer>
+        <router-view></router-view>
+    </v-content>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            sidebar: false,
+            menu: [
+                { icon: 'assignment_turned_in', name: 'Sing Up online', path: 'teacherResults' },
+                { icon: 'description', name: 'About', path: 'teacherResults' },
+                { icon: 'account_box', name: 'Personal', path: 'teacherResults' },
+                { icon: 'mail', name: 'Contacts', path: 'teacherResults' },
+                { icon: 'comment', name: 'Reviews', path: 'teacherResults' },
+                { icon: 'question_answer', name: 'FAQ', path: 'teacherResults' },
+            ]
+        }
+    }
+}
+</script>
+<style scoped>
+.sidebar{
+    padding: 1rem;
+}
+.sidebar-title h1{
+    display: flex;
+}
+.sidebar-title i{
+    padding-right: .4rem;
+}
+.menu a{
+    text-decoration: none;
+}
+.menu{
+    margin-top: 1rem;
+    font-size: 2rem !important;
+}
+.menu-item{
+    margin-top: 1rem;
+}
+</style>
